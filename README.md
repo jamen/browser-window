514608a5ee2b802a6d09028bdb1b3ebda8e9273e
Jamen Marz <jamenmarz@gmail.com>
2016-10-19 13:10:33 -0600

    Small progress

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # browser-window [![NPM version](https://badge.fury.io/js/browser-window.svg)](https://npmjs.org/package/browser-window) [![Build Status](https://travis-ci.org/jamen/browser-window.svg?branch=master)](https://travis-ci.org/jamen/browser-window)
 
-> A better `BrowserWindow` for Electron.
+> A helpful `BrowserWindow` wrapper.
 
 ## Installation
 
--- index.js
+++ index.js
@@ -1,4 +1,65 @@
-'use strict';
+browser.frameless = frameless
+browser.transparent = transparent
+module.exports = browser
+const { BrowserWindow, ipcMain } = require('electron')
+const EventEmitter = require('events')
+const fastArgs = require('fast-args')
+const assign = require('assign-deep')
 
-module.exports = function() {
-};
+/**
+ * Normal window
+ */
+function browser (windowOptions) {
+  // Add defaults to window options.
+  // windowOptions = assign({}, windowOptions)
+
+  // Create window back-end with options
+  const window = new BrowserWindow(windowOptions)
+
+  // Load a URL, buffer, string, etc.
+  function load (source, options) {
+    options = assign({
+      data: null,
+      encoding: 'base64'
+    }, options)
+
+    // Encode to data URI
+    if (options.data) {
+      const data = Buffer.from(source).toString(options.encoding)
+      window.loadURL(`data:${options.data};${options.encoding},${data}`)
+    } else {
+      window.loadURL(source)
+    }
+  }
+
+  // Send an IPC message
+  function send (...args) {
+    return window.webContents.send(...args)
+  }
+
+  return { load, send }
+}
+
+/**
+ * Frameless window
+ */
+function frameless (options) {
+  options = Object.assign({
+    frame: false
+  }, options)
+
+  // Create window
+  return browser(options)
+}
+
+/**
+ * Transparent window
+ */
+function transparent (options) {
+  options = Object.assign({
+    transparent: true
+  }, options)
+
+  // Create window
+  return frameless(options)
+}
